d8531cc6a585d947818be95b1a1e0dbbbef9b93f
dan sp3 <danielanderson21@gmail.com>
2016-08-26 11:32:24 -0400

    Began login logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :before_filters
+
+  def before_filters
+    if !session[:logged_in]
+      redirect_to '/sessions/login'
+      return false
+    end
+  end
+
 end
