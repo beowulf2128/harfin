d2325608393c4d7f70f13b2a903860243244f1e3
dan sp3 <danielanderson21@gmail.com>
2016-10-30 16:55:58 -0400

    Added Sessionyear#current

--- app/models/sessionyear.rb
+++ app/models/sessionyear.rb
@@ -3,4 +3,8 @@ class Sessionyear < ApplicationRecord
   validates_presence_of :start_date
   validates_presence_of :end_date
 
+  #
+  def self.current
+    where(['start_date < ? and end_date > ?', Time.now, Time.now]).first
+  end
 end
