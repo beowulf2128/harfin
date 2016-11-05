010a0ccee71fcbff7d93dd868c368cfeb477bf88
dan sp3 <danielanderson21@gmail.com>
2016-11-05 19:31:15 -0400

    time formats added as initializer

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,5 @@
+Time::DATE_FORMATS[:year] = '%Y'                     # 2016
+Time::DATE_FORMATS[:mdy] = '%m/%d/%Y'                # 10/20/2016
+Time::DATE_FORMATS[:mdyhm] = '%m/%d/%Y %I:%M %p'     # 10/20/2016 7:20 PM
+Time::DATE_FORMATS[:mdyhms] = '%m/%d/%Y %I:%M:%S %p' # 10/20/2016 7:20:32 PM
+
