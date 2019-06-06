09a58a38dbc35a8a387d718b3f33897e41a9c3d5
DA <danielanderson21@gmail.com>
2019-06-05 21:09:37 -0400

    Move erd.pdf to tmp

--- app/controllers/utils_controller.rb
+++ app/controllers/utils_controller.rb
@@ -2,8 +2,8 @@ class UtilsController < ApplicationController
 
   def erd
     raise 'Unauthorized action' unless Rails.env == 'development'
-    system 'rake erd' #unless File.exists? 'erd.pdf'
-    erd_pdf = IO.read('erd.pdf')
+    system "rake erd filename=tmp/erd" #unless File.exists? 'erd.pdf'
+    erd_pdf = IO.read('tmp/erd.pdf')
     send_data erd_pdf, file_name: 'erd.pdf', type: 'application/pdf', disposition: 'attachment'
   end
 end
